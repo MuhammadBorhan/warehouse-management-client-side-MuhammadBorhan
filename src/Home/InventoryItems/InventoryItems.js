c745ee5e95f5ba4e044161b2ea3f3eaecb8ce909
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-01 16:30:21 +0600

    handle stock update

--- src/Home/InventoryItems/InventoryItems.js
+++ src/Home/InventoryItems/InventoryItems.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const InventoryItems = ({ product }) => {
-    const { name, price, quantity, img, supplierName, description } = product;
+    const { _id, name, price, quantity, img, supplierName, description } = product;
     return (
         <div className="col-md-4 g-4">
             <div className='shadow p-3 text-center'>
@@ -13,7 +13,7 @@ const InventoryItems = ({ product }) => {
                 <p className='m-0'>Supplier: {supplierName}</p>
                 <p>{description}</p>
                 <button>
-                    <Link className='text-decoration-none text-xl rounded bg-sky-700 text-white fw-bold px-3 py-1 ' to='/update'>Update</Link>
+                    <Link className='text-decoration-none text-xl rounded bg-sky-700 text-white fw-bold px-3 py-1 ' to={`/update/${_id}`}>Stock Update</Link>
                 </button>
             </div>
         </div>
--- src/Home/Update/Update.js
+++ src/Home/Update/Update.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 
 const Update = () => {
+    const [product, setProduct] = useState({});
+    const { id } = useParams();
+    const { img, _id, name, price, quantity, supplierName, description } = product;
+
+    useEffect(() => {
+        const url = `http://localhost:5000/update/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, [id]);
+
+    const handleDecrement = () => {
+        setProduct(quantity - 1)
+    }
+
     return (
-        <div className='container'>
-            <h3>This is update page</h3>
+        <div className='container py-3'>
+            <div className="row">
+                <div className="col-12 col-md-6 mx-auto">
+                    <Card className='p-3 text-center'>
+                        <Card.Img variant="top" src={img} />
+                        <Card.Body>
+                            <Card.Title>{name}</Card.Title>
+                            <Card.Text>{description}</Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>Price: ${price}</ListGroupItem>
+                            <ListGroupItem>Quantity: {quantity}</ListGroupItem>
+                            <ListGroupItem>Supplier: {supplierName}</ListGroupItem>
+                            <ListGroupItem>Id: {_id}</ListGroupItem>
+                        </ListGroup>
+                        <Card.Body>
+                            <button onClick={() => handleDecrement()} className='btn btn-primary'>Deliver</button>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </div>
             <button className='my-5 mx-auto d-block'>
                 <Link to='/manageitem' className='text-decoration-none rounded bg-blue-700 text-xl p-2 text-white fw-bold'>Manage Inventory</Link>
             </button>
